dcde5d268ceb58e64c03c65f2ae6981cf4b0d1e0
dhirajshah04 <dhiraj.shah04@gmail.com>
2017-09-06 23:03:37 +0545

    Save username and password

--- guardme.py
+++ guardme.py
@@ -5,6 +5,7 @@ import os
 
 login = {}
 
+
 if not os.path.exists("user.txt"):
     open("user.txt", 'w').close()
 
@@ -15,76 +16,79 @@ def lddict():
     except EOFError:
         None
         return dict()
-              
-      
-print("\n")
-print("Please select the operation Number\n")
-print("1. Save Username and Password")
-print("2. Retrieve Password")
-print("3. Exit\n")
-choice = input("Enter your Choice: ").strip()
 
-while True:
-        if choice == "1":
-            login = lddict()
-            username = input("\nEnter Your username: ").strip()
+def menu():
+    print("\n")
+    print("\t\t MENU")
+    print("\t\t==================================\n")
+    print("\t\tPLEASE SELECT THE OPERATION NUMBER\n")
+    print("\t\t1. Save Username and Password")
+    print("\t\t2. Retrieve Password")
+    print("\t\t3. Exit\n")
+    print("\t\t==================================\n")
+    choice = input("Enter your Choice: ").strip()
 
-            while username in login.keys():
-                print("username already Exist")
-                username = input("\nEnter Your Username: ").strip()
-            else:
+    while True:
+            if choice == "1":
+                login = lddict()
+                username = input("\nEnter Your username: ").strip()
+                while username in login.keys():
+                    print("\t\tusername already Exist")
+                    username = input("\nEnter Your Username: ").strip()
+
+                while username == "":
+                    print("\n\t\tPlease Enter a valid Username\n")
+                    username = input("Enter Your username: ").strip()
+
                 password = input("Enter Your Password: ").strip()
+                while password == "":
+                    print("\n\t\tPleaser Enter a valid Password\n")
+                    password = input("Enter Your Password: ").strip()
 
-            
-            while username == "" or password == "":
-                print("please Enter a valid Username and password:")
-                username = input("Enter Your username: ").strip()
-                password = input("Enter Your Password: ").strip()
+                login[username] = password
 
-            
-            login[username] = password
-                        
-            
-            fo = open("user.txt", 'wb')
-            pickle.dump(login,fo)
-            fo.close()
+                fo = open("user.txt", 'wb')
+                pickle.dump(login,fo)
+                fo.close()
 
-            print("\n Your Username and Password has been Saved")
-            cont = input("\nDo you want to continue (y/n) ?\n").strip()
-            if cont == "y" or cont == "Y":
-                print("\n")
-                print("Please Select the operation number")
-                print("\n")
-                print("1. Save Username and Password")
-                print("2. retrieve Password")
-                print("3.Exit")
-                print("\n")
-                choice = input("Enter your choice: ").strip()
+                print("\n Your Username and Password has been Saved")
+                cont = input("\nDo you want to continue (y/n) ?\n").strip()
+                if cont == "y" or cont == "Y":
+                    menu()
 
-            elif cont == "n" or cont == "N":
-                print ("\t Thank you")
+                elif cont == "n" or cont == "N":
+                    print ("\t Good Bye\n")
+                    sys.exit(0)
+
+            elif choice == "2":
+                login = lddict()
+                print("\n\n\t\tYou Are About To Retrieve Your PASSWORD \n")
+                r_username = input("Enter Your username: ").strip()
+                            
+                            
+                if r_username in login.keys():
+                    pyperclip.copy(login.get((r_username)))
+                    print("\n\t\tYour PASSWORD Has Been Copied To Clipboard\n\t Use Ctrl + V And Paste Your Password To Your Desired Location.\n")
+                else:
+                    print("username not found")
+
+                cont1 = input("\nDo you want to continue (y/n) ?\n").strip()
+                if cont1 == "y" or cont1 == "Y":
+                    menu()
+
+                elif cont1 == "n" or cont == "N":
+                    print ("\t Good Bye\n")
+                    sys.exit(0)
+
+            elif choice == "3":
+                print("\n\tGood Bye\n")
                 sys.exit(0)
 
-        elif choice == "2":
-            login = lddict()
-            print("\n You are about to retrieve your password \n")
-            r_username = input("Enter Your username: ").strip()
-                        
-                        
-            if r_username in login.keys():
-                #print("The Passowrd is: ") ,
-                #...
-                #print (login.get(r_username)),  #.get() is used to retrieve password or retrieve value of key
-                #r_password = login[username]
-                pyperclip.copy(login.get((r_username)))
-                print("Your password has been copied to clipboard")
-            #f.close()
-            else:
-                print("username not found")
-            break
+            elif choice == "": #and choice >= "4":
+                print("\n\n\t\tPLEASE ENTER A VALID CHOICE NUMBER\n")
+                menu()
 
-        elif choice == "3":
-            print("\n\tThank You\n")
-            sys.exit(0)
-
-
+            elif choice >= "4":
+                print("\n\n\t\tPLEASE ENTER A VALID CHOICE NUMBER\n")
+                menu()
+menu()
